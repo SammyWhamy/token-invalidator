9181b95926d9e1c0d17d213a86f19da38a9a0bef
nance <73056456+the-nance@users.noreply.github.com>
2022-06-11 22:54:33 +0300

    Update Form.tsx

--- components/Form.tsx
+++ components/Form.tsx
@@ -2,12 +2,12 @@ import { info } from "../data/config"
 import TextInput from "./TextInput"
 import LoggedIn from "./LoggedIn";
 import LoginButton from "./LoginButton";
-
+import Link from "next/link"
 export default function Form({ data }) {
     return (
         <div className='h-screen relative bg-gradient-to-br from-blue-101 to-fuchsia-101 overflow-hidden'>
 
-            <div className='w-11/12 md:w-3/6 2xl:w-2/6 fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 backdrop-blur-10px bg-white/30 rounded-lg md:p-20 p-10'>
+            <div className='w-11/12 md:w-3/6 2xl:w-2/6 fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 backdrop-blur-10px bg-gray-700/30 backdrop-opacity-xl rounded-lg md:p-20 p-10'>
                 {data ? (<LoggedIn data={data} />) : (<LoginButton/>)}
                 <h1 className="text-white text-center font-semibold text-3xl mb-8">
                     {info.title}
@@ -15,14 +15,14 @@ export default function Form({ data }) {
                 <div className="overflow-x-hidden w-full">
                     <h2 className="landingSectionTitle max-w-max mx-0 text-left relative mb-10 md:w-max"></h2>
                 </div>
-                <p className="text-white text-xl mb-5">
+                <div className="text-white text-xl mb-5">
                     <span className="text-white text-xl">{info.subHeading}</span>
                     {info.lists.map((i, index) => {
                         return (
-                            <li key={index}>{i}</li>
+                            <li key={index}><Link href = {i.url || "#"}>{i.label}</Link></li>
                         )
                     })}
-                </p>
+                </div>
                 <TextInput data={data}/>
             </div>
         </div>
