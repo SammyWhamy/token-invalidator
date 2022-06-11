c2cf878b2d3c8d4e4537ed2b57a4e9936eae7340
Sam Teeuwisse <sam.teeuwisse2@gmail.com>
2022-06-11 20:42:44 +0200

    Remove console log

--- pages/api/tokens.ts
+++ pages/api/tokens.ts
@@ -4,8 +4,6 @@ const prisma = new PrismaClient();
 export default async function handler(req, res) {
     if(req.method !== 'GET') return res.status(405);
 
-    console.log(process.env)
-
     const tokens = await prisma.token.findMany();
     res.json(tokens);
 }
