9d989eb1e8e5883a0d99af5848a17f89cc3feb93
nance <73056456+the-nance@users.noreply.github.com>
2022-06-11 19:26:20 +0300

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,27 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: [
-        "./pages/**/*.{js,ts,jsx,tsx}",
-        "./components/**/*.{js,ts,jsx,tsx}",
-    ],
-    theme: {
-        extend: {},
+  mode: 'jit',
+  content: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  darkMode: 'class',
+  theme: {
+    fontFamily: {
+      sans: ['Times new roman']
     },
-    plugins: [],
-}
+    extend: {
+      colors: {
+        dark: '#000',
+        white: '#fff',
+        lightText: '#76797d',
+        bgDark: '#2E3440',
+        cardDark: '#434C5E',
+        butDark: '#5E81AC',
+        bgLight: '#D8DEE9',
+        cardLight: '#E5E9F0',
+        butLight: '#81A1C1'
+      }
+    }
+  },
+  variants: {
+    backgroundColor: ({ after }) => after(["disabled"]),
+  },
+  plugins: []
+};
