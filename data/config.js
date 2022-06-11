425de2c96fdd4c551951b8022cfab79b44bc6924
nance <73056456+the-nance@users.noreply.github.com>
2022-06-11 19:23:27 +0300

    Create config.js

---
+++ data/config.js
@@ -0,0 +1,6 @@
+export const info = {
+  title: "Token Invalidator",
+  subHeading: "If you got a system DM saying your token was found here, it's because I found your bot's token.",
+  lists: ["invalidate tokens by submitting them here","invalidate tokens by submitting them here","invalidate tokens by submitting them here","invalidate tokens by submitting them here","invalidate tokens by submitting them here",],
+  scopes: ["identify"]
+}
