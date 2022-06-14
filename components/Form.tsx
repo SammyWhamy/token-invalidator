97a22daec0445da8d14dcde2b8d6f15522e04508
nance <73056456+the-nance@users.noreply.github.com>
2022-06-14 10:45:46 +0300

    Update Form.tsx
    added fade in

--- components/Form.tsx
+++ components/Form.tsx
@@ -7,7 +7,7 @@ import Link from "next/link"
 export default function Form({ data }) {
     return (
         <div className='h-screen relative bg-gradient-to-br from-blue-101 to-fuchsia-101 overflow-hidden'>
-            <div className='w-11/12 md:w-3/6 2xl:w-6/12 fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 backdrop-blur-10px bg-gray-700/30 backdrop-opacity-xl rounded-3xl md:p-20 p-10'>
+            <div className='animate-slide w-11/12 md:w-3/6 2xl:w-6/12 fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 backdrop-blur-10px bg-gray-700/30 backdrop-opacity-xl rounded-3xl md:p-20 p-10'>
                 <div className="translate-y-[-3.5rem] translate-x-[-3.5rem]">
                     {data ? (<LoggedIn data={data} />) : (<LoginButton />)}
                 </div>
