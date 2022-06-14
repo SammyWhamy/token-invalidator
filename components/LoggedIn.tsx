b677d84697d0f0b68c05ee907c2fbbc02a6f6ac0
Sam Teeuwisse <sam.teeuwisse2@gmail.com>
2022-06-14 12:33:21 +0200

    Restyle logout button

--- components/LoggedIn.tsx
+++ components/LoggedIn.tsx
@@ -1,21 +1,25 @@
-import {faClockRotateLeft} from "@fortawesome/free-solid-svg-icons";
+import {faClockRotateLeft, faArrowRightFromBracket} from "@fortawesome/free-solid-svg-icons";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import Link from 'next/link';
+
 export default function LoggedIn({ data }) {
     return (
         <div className="inline-flex">
-            <Link href="/api/logout">
-                <button className="font-semibold py-2 pr-[0.95rem] px-4 rounded-full mr-1 inline-flex items-center text-white bg-bgDark focus:outline-none">
-                    <img
-                        className="w-8 h-8 mr-2 ml-[-0.6rem] mt-[-100%] mb-[-100%]"
-                        src={`https://cdn.discordapp.com/avatars/${data?.id}/${data?.avatar}.webp`}
-                        alt={data?.username}
-                        style={{ borderRadius: '50%', position: 'relative', textAlign: 'center' }} />
-                    <span>
-                        {data ? `${data.username}#${data.discriminator} | Log out` : ""}
-                    </span>
-                </button>
-            </Link>
+            <button className="cursor-default font-semibold py-2 pr-[0.95rem] px-4 rounded-full mr-1 inline-flex items-center text-white bg-bgDark focus:outline-none">
+                <img
+                    className="w-8 h-8 mr-2 ml-[-0.6rem] mt-[-100%] mb-[-100%]"
+                    src={`https://cdn.discordapp.com/avatars/${data?.id}/${data?.avatar}.webp`}
+                    alt={data?.username}
+                    style={{ borderRadius: '50%', position: 'relative', textAlign: 'center' }} />
+                <span>
+                    {data ? `${data.username}#${data.discriminator}` : ""}
+                    {data ? (
+                        <Link href="/api/logout">
+                            <FontAwesomeIcon icon={faArrowRightFromBracket} size="lg" className="text-red-400 cursor-pointer pl-2 translate-y-[0.05rem]"/>
+                        </Link>
+                    ) : ""}
+                </span>
+            </button>
 
             <Link href={`/history/user/${data.id}`}>
                 <button className="ml-1 font-semibold py-2 pr-3 px-4 rounded-full inline-flex items-center text-white bg-bgDark focus:outline-none">
