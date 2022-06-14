2d38ce0bac71f20086fd800d68bb5cf603d3c455
nance <73056456+the-nance@users.noreply.github.com>
2022-06-14 11:42:51 +0300

    Update LoggedIn.tsx

--- components/LoggedIn.tsx
+++ components/LoggedIn.tsx
@@ -14,7 +14,7 @@ export default function LoggedIn({ data }) {
           </span>
         </button>
       </Link>
-      <Link href={`/pages/history/users/${data.id}`}>
+      <Link href={`/history/users/${data.id}`}>
         <button className="font-semibold py-2 pr-3 px-4 rounded-full mr-1 inline-flex items-center text-white bg-bgDark focus:outline-nonez">
           <img
             className="w-8 h-8 mr-2 ml-[-0.6rem] mt-[-100%] mb-[-100%]"
