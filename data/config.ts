96da4f44642a955598bddc0954b442386f875052
nance <73056456+the-nance@users.noreply.github.com>
2022-06-14 10:32:51 +0300

    Update config.ts

--- data/config.ts
+++ data/config.ts
@@ -13,6 +13,10 @@ export const config = {
             label: "Token repo",
             url: "https://github.com/SammyWhamy/invalidate-tokens"
         },
+        {
+          label: "History",
+          url: "/history"
+        }
         // {
         //     label: "Contact us"
         // },
