3c59c22638ec78a27415d3cd9127772b899e5317
Sam Teeuwisse <sam.teeuwisse2@gmail.com>
2022-06-14 10:45:22 +0200

    Fix prod x2

--- pages/history/[id].tsx
+++ pages/history/[id].tsx
@@ -1,7 +1,7 @@
-import Table from '../../../components/Table';
 import Head from 'next/head'
-import { useRouter } from "next/router";
+import {useRouter} from "next/router";
 import useSWR from 'swr'
+import Table from '../../components/Table';
 
 export default function GetUser() {
     const router = useRouter();
@@ -9,19 +9,18 @@ export default function GetUser() {
     const { error, data } = useSWR(id ? `https://token-invalidator.vercel.app/api/tokens/user/${id}` : null);
     if (error) return <div>Failed to load</div>
     if (!data) return <div>Loading...</div>
-    const tokenInfo = data
     return (
         <>
             <Head>
                 <title>{id} - History</title>
             </Head>
             <div className='h-screen w-screen fixed bg-gradient-to-br from-blue-101 to-fuchsia-101 overflow-auto'>
-                <div className='rounded-3xl m-5 w-5/6 mx-auto backdrop-blur-10px bg-white/30 backdrop-opacity-xl rounded-3xl'>           
+                <div className='rounded-3xl m-5 w-5/6 mx-auto backdrop-blur-10px bg-white/30 backdrop-opacity-xl rounded-3xl'>
                     <h1 className="text-white text-center justify-self-center font-semibold text-3xl mb-8 px-3 py-3">
                         YOUR Token History
                     </h1>
                 </div>
-                <Table tokenData={tokenInfo} />
+                <Table tokenData={data} />
             </div>
         </>
     );
