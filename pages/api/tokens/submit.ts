2f818eb51e7204049d0d822a4fe82654ed442bee
Sam Teeuwisse <sam.teeuwisse2@gmail.com>
2022-06-18 23:06:57 +0200

    Fix incorrectly reporting token types

--- pages/api/tokens/submit.ts
+++ pages/api/tokens/submit.ts
@@ -38,7 +38,7 @@ export default async function handler(req, res) {
             token: token,
             link: link || "N/A",
             submitter: auth.submitter,
-            type: true// tokenType.type
+            type: tokenType.type
         }
     });
 
