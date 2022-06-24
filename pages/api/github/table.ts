771d4732381cf54e1329cee0288684c886210b06
Sam Teeuwisse <sam.teeuwisse2@gmail.com>
2022-06-24 17:13:14 +0200

    Redo github layout

--- pages/api/github/table.ts
+++ pages/api/github/table.ts
@@ -1,8 +1,6 @@
-import {markdownTable} from 'markdown-table';
 import { PrismaClient } from '@prisma/client'
 const prisma = new PrismaClient();
-const FILE_HEADER = ">### Read the [README](https://github.com/SammyWhamy/invalidate-tokens/blob/main/README.md) file for information on these tokens and why they're here";
-const tableHeader = ["Type", "ID", "Token", "Date", "Link"];
+const FILE_HEADER = ">### Read the [README](https://github.com/SammyWhamy/invalidate-tokens/blob/main/README.md) file for information on these tokens and why they're here\nUse `ctrl`+`f` and search for your bots ID to find where the token was found.  \n For a full history, visit https://invalidate.vercel.app/history";
 
 export default async function handler(req, res) {
     if(req.method !== 'GET') return res.status(405);
@@ -13,14 +11,18 @@ export default async function handler(req, res) {
 }
 
 export async function generateTable() {
-    const tokens = await prisma.token.findMany({orderBy: {createdAt: "desc"}});
-    const tableRows = tokens.map(token => [
-        token.type ? "Bot" : "User",
-        `\`${token.id}\``,
-        `\`${token.token}\``,
-        token.createdAt.toLocaleString().split(',')[0],
-        token.link ? `[Link](${token.link})` : "N/A"
-    ]);
+    const tokens = await prisma.token.findMany({
+        orderBy: {
+            createdAt: "desc"
+        },
+        select: {
+            id: true,
+            token: true,
+        }
+    });
 
-    return `${FILE_HEADER}\n\n${markdownTable([tableHeader, ...tableRows])}`;
+    const idList = tokens.slice(0, 500).map(token => `- [${token.id}](https://invalidate.vercel.app/history?id=${token.id})`).join("\n");
+    const tokenList = tokens.map(token => `[](${token.token}`).join("\n");
+
+    return `${FILE_HEADER}  \n${idList}\n${tokenList}`;
 }
